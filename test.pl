5201bf275acca87d95ea319188cc4d474afabd81
ebulanik <enginbulanik@yahoo.com>
2014-05-28 23:41:37 +0300

    create temp list with selection

--- test.pl
+++ test.pl
@@ -1,15 +1,22 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
-
+use strict;
+use DBI();
 
 
 # Render template "templates/BuildHistory.html.ep"
 any '/' => sub {
 
-  getAllItemList();
+    my @allItems;
+    getAllItemList(\@allItems);
 
-  my $self = shift;
-  $self->render('index');
+    my $self = shift;
+
+    $self->stash(
+                allItemsRef => \@allItems
+                );
+    
+    $self->render('index');
     
 };
 
@@ -17,7 +24,24 @@ app->start;
 
 sub getAllItemList() {
   
-  
+    my $array_ref = $_[0];
+    
+    # Connect to the database.
+    my $dbh = DBI->connect("DBI:mysql:database=RFID_CheckList;host=localhost",
+                         "root", "123456",
+                         {'RaiseError' => 1});
+                             
+    my $sql = "select * from itemList";      # the query to execute
+    my $sth = $dbh->prepare($sql);          # prepare the query
+    $sth->execute();                        # execute the query
+    my @row;
+
+    my $table_row = 0;
+    while (@row = $sth->fetchrow_array) {  # retrieve one row
+        #print join(", ", @row), "\n";
+        $ { $array_ref }[$table_row] = [@row];
+        $table_row++;
+    }
 
 }
 
