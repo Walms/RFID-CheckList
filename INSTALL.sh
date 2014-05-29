79436ab41fb97bbb153acee7ed47b72150a5cc41
ebulanik <enginbulanik@yahoo.com>
2014-05-29 22:49:18 +0300

    fix install

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,5 +3,5 @@ wait
 mysql -uroot -p123456 -e "CREATE DATABASE IF NOT EXISTS RFID_CheckList"
 mysql -uroot -p123456 -e "DROP TABLE itemList" RFID_CheckList
 mysql -uroot -p123456 -e "CREATE TABLE IF NOT EXISTS itemList (tagNumber INT, equipmentType VARCHAR(50), Maker VARCHAR(50), Model VARCHAR(50), serialNumber VARCHAR(50), tmpListCnt INT)" RFID_CheckList
-mysql -uroot -p123456 "RFID_CheckList" -e "LOAD DATA INFILE 'exampleDatabaseEntries.csv' INTO TABLE itemList  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES"
+mysql -uroot -p123456 "RFID_CheckList" -e "LOAD DATA INFILE 'exampleDatabaseEntries.csv' INTO TABLE itemList  FIELDS TERMINATED BY ',' IGNORE 1 LINES"
 morbo index.pl
