08a5cbe8c0de2905a5eca4c1b99f4cd287dd0d5d
Daniel Walmsley <daniel.walmsley@gmail.com>
2014-07-15 18:26:33 +1200

    Correcting the INSTALL.sh, there was an error in the creation of the Mysql db. The csv was place in the wrong location preventing the db from being created

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,9 @@
-sudo cp exampleDatabaseEntries.csv /var/lib/mysql/RFID_CheckList
+sudo su mysql mkdir /var/lib/mysql/RFID_CheckList
 wait
 mysql -uroot -p123456 -e "CREATE DATABASE IF NOT EXISTS RFID_CheckList"
 mysql -uroot -p123456 -e "DROP TABLE itemList" RFID_CheckList
 mysql -uroot -p123456 -e "CREATE TABLE IF NOT EXISTS itemList (tagNumber INT, equipmentType VARCHAR(50), Maker VARCHAR(50), Model VARCHAR(50), serialNumber VARCHAR(50), tmpListCnt INT, tagDetected INT)" RFID_CheckList
+sudo cp exampleDatabaseEntries.csv /var/lib/mysql/RFID_CheckList/
+sudo chown mysql:mysql /var/lib/mysql/RFID_CheckList/exampleDatabaseEntries.csv
 mysql -uroot -p123456 "RFID_CheckList" -e "LOAD DATA INFILE 'exampleDatabaseEntries.csv' INTO TABLE itemList  FIELDS TERMINATED BY ',' IGNORE 1 LINES"
 morbo index.pl
